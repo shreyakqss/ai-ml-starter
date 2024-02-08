2e45abc29b5bf2aad6e98228dd59b39bdd1f3a4a
Shreyak Chakraborty <shreyak.chakraborty@qsstechnosoft.com>
2024-02-08 18:27:52 +0530

    multi-model support

--- langchain-streamlit/llm_functions.py
+++ langchain-streamlit/llm_functions.py
@@ -2,21 +2,34 @@
 from langchain.llms.ctransformers import CTransformers
 from langchain.chains import LLMChain
 from langchain.prompts import PromptTemplate
+from langchain.chat_models.google_palm import ChatGooglePalm
+from langchain.chat_models.cohere import ChatCohere
+import os
 
-def initLLM():
+def initLLM(model_name):
     # llm = Bedrock(
     #     credentials_profile_name="bedrock-admin", model_id="amazon.titan-text-express-v1"
     # )
 
-    # load a quantized version of LLama2 on CPU
-    # main link: https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML
-    # llama2 quantized models comparison:nhttps://www.reddit.com/r/LocalLLaMA/comments/14gjz8h/i_have_multiple_doubts_about_kquant_models_and/
-    llm = CTransformers(model = "../model/llama-2-7b-chat.ggmlv3.q5_K_S.bin",
-        model_type = "llama",
-        max_new_tokens=512,
-        temperature=0.2
-        
+    if model_name == "llama-2-7b-chat":
+        # load a quantized version of LLama2 on CPU
+        # main link: https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML
+        # llama2 quantized models comparison:nhttps://www.reddit.com/r/LocalLLaMA/comments/14gjz8h/i_have_multiple_doubts_about_kquant_models_and/
+        llm = CTransformers(model = "../model/llama-2-7b-chat.ggmlv3.q5_K_S.bin",
+            model_type = "llama",
+            max_new_tokens=512,
+            config={
+                'context_length': 2048
+            },
+            temperature=0.2    
         )
+    elif model_name == "cohere":
+        llm  = ChatCohere(cohere_api_key=os.environ['COHERE_API_KEY'])
+    elif model_name == "google-palm":
+        llm  = ChatGooglePalm(google_api_key=os.environ['GOOGLE_API_KEY'])
+
+    else:
+        raise Exception("Invalid model name")
 
     return llm
 
@@ -25,8 +38,8 @@ def initLLM():
 def ragProcess():
     return None 
 
-def getLLMResponse(query: str):
-    llm = initLLM()
+def getLLMResponse(query: str,model_name: str):
+    llm = initLLM(model_name)
     prompt_template = """
     You are an expert article writer for a wide range of topics.
     Given this article text: {query}
--- langchain-streamlit/main.py
+++ langchain-streamlit/main.py
@@ -1,23 +1,39 @@
 import streamlit as st
 from llm_functions import getLLMResponse
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+supported_models = {
+    # 'OpenAI GPT-4':"gpt-4",
+    'Llama 2 7B-Chat GGML (Quantized)': 'llama-2-7b-chat',
+    'Cohere':"cohere",
+    'Google PaLM (Gemini)':"google-palm",
+}
 
 st.header("Langchain Streamlit test")
-# st.write("Model: Llama-2-70b (Bedrock)")
-st.write("Model: Llama-2-7b-Chat (Local)")
+
+# file_content = st.file_uploader("Upload PDF",type=".pdf")
+# if file_content is not None:
+#     print("file_content",file_content)
 
 
-file_content = st.file_uploader("Upload PDF",type=".pdf")
-if file_content is not None:
-    print("file_content",file_content)
+article_text = st.text_area("Enter article text:")
+
+# Radio button to switch between models
+model_choice = st.radio(
+    label="Choose an LLM model:",
+    options=list(supported_models.keys())
+)
 
 
-user_input = st.text_input("Enter question:")
-if st.button("Submit"):
+if st.button("Generate new article"):
     try:
-        llm_response = getLLMResponse(user_input)
+        llm_response = getLLMResponse(article_text,model_name=supported_models[model_choice])
         if llm_response is not None:
             st.write(llm_response['text'])
         else:
-            raise Exception("RAG Failed")
+            raise Exception("LLM Failed Failed")
     except Exception as e:
         st.error(e)
